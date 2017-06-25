f512fd132adbcad3515c531e26458a464743c0a8
wangchao <wangchaobjdx@163.com>
2017-06-25 15:51:47 +0800

    提交feign-hystrix项目

---
+++ microservice-consumer-parent/microservice-consumer-movie-feign-hystrix-fallback-stream/src/main/java/com/cycle/springcloud/user/ConsumerMovieApplication.java
@@ -0,0 +1,17 @@
+package com.cycle.springcloud.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableCircuitBreaker
+public class ConsumerMovieApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerMovieApplication.class, args);
+	}
+}
---
+++ microservice-consumer-parent/microservice-consumer-movie-feign-hystrix-fallback-stream/src/main/java/com/cycle/springcloud/user/feign/UserFeignClient.java
@@ -0,0 +1,26 @@
+package com.cycle.springcloud.user.feign;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.cycle.springcloud.user.entity.User;
+
+@FeignClient(name = "microservice-provider-user",fallback = FeignClientFallback.class)
+public interface UserFeignClient {
+	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
+	User findById(@PathVariable("id") Long id);
+}
+
+@Component
+class FeignClientFallback implements UserFeignClient {
+	@Override
+	public User findById(Long id) {
+		User user = new User();
+		user.setId(-1L);
+		user.setUsername("默认用户");
+		return user;
+	}
+}
